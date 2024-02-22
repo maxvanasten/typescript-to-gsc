067f84d25a54060b61b298456d89c8d3bff883d6
Max van Asten <maxvasten@gmail.com>
2024-02-22 22:19:31 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -8,11 +8,20 @@ const ts_gsc_config = {
 				'./scripts/src/zm/origins_spawnroom/main.ts',
 				// Hud element
 				'./scripts/src/zm/origins_spawnroom/hud.ts',
+				// Random perk from generator script
+				'./scripts/src/zm/origins_generator_challenge/main.ts',
 				// Add health meter and zombie counter
 				'./scripts/src/zm/health_counter/main.ts',
 				'./scripts/src/zm/zombie_counter/main.ts'
 			]
 		}
+		// {
+		// 	name: 'origins_generator',
+		// 	output_file: './scripts/output/zm/zm_tomb/origins_generator.gsc',
+		// 	input_files: [
+		// 		'./scripts/src/zm/origins_generator_challenge/main.ts'
+		// 	]
+		// }
 	]
 };
 
